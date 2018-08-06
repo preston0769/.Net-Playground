b545ab593cca8581c4fc62e42b839bb990f42224
Preston Zhang <preston0769@gmail.com>
2018-08-06 22:57:09 +1000

    Simple Fibonacci Unit Test
    First really simple Fibonacity Unit Test

---
+++ FibonacciProject.Tests/FibonacciUnitTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciProject.Tests
+{
+    [TestClass]
+    public class MyTestClass
+    {
+
+        [TestMethod]
+        public void Fib_Given0_Return0()
+        {
+            int n = 0;
+            int result = Fibonacci.Fib(n);
+            Assert.AreEqual(n, result);
+        }
+
+        [TestMethod]
+        public void Fib_Given1_Return1()
+        {
+            int n = 1;
+            int result = Fibonacci.Fib(n);
+            Assert.AreEqual(1, result);
+        }
+        
+        [TestMethod]
+        public void Fib_Given2_Return1()
+        {
+            int n = 2;
+            int result = Fibonacci.Fib(n);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Fib_Given3_Return2()
+        {
+            int n = 3;
+            int result = Fibonacci.Fib(n);
+            Assert.AreEqual(2, result);
+        }
+
+    }
+}
---
+++ FibonacciProject/Fibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciProject
+{
+    public static class Fibonacci
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n <= 2)
+                return 1;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
