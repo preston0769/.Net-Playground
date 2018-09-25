ba87aaee8adb42c4db2b5f32c00a5e29b78c970a
Preston Zhang <preston0769@gmail.com>
2018-09-25 21:44:28 +1000

    Restructure & add identity server demo
    Make this repo a place to play .net related files

---
+++ IdentityServer/IdentityServer4Demo/ClientApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            MainAsync().GetAwaiter().GetResult();
+
+            Console.ReadLine();
+
+        }
+
+
+        private static async Task MainAsync()
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+
+            var client = new HttpClient();
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:5001/api/Identity");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
